e2ea6af7f916c4550245f1cd93ac05c72fdb606f
sanjayyadavgithub <48355724+sanjayyadavgithub@users.noreply.github.com>
2021-10-10 08:29:46 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Switch, Route } from 'react-router-dom';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart/";
+import Modal from './components/Modal';
+
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route exact path="/details" component={Details} />
+          <Route exact path="/cart" component={Cart} />
+          
+        </Switch>
+        <Modal />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,59 @@
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: 'Food Card',
+    img: 'https://react-coding-assignment.s3.ap-south-1.amazonaws.com/cards/orange_card.png',
+    price: 21.00,
+    company: 'GOOGLE',
+    info: 'This card is used for spending on food merchants',
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 2,
+    title: 'Food Card',
+    img: 'https://react-coding-assignment.s3.ap-south-1.amazonaws.com/cards/blue_card.png',
+    price: 21.00,
+    company: 'SAMSUNG',
+    info: 'This card is used for spending on food merchants',
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 3,
+    title: 'Food Card',
+    img: 'https://react-coding-assignment.s3.ap-south-1.amazonaws.com/cards/golden_card.png',
+    price: 21.00,
+    company: 'htc',
+    info: 'This card is used for spending on food merchants',
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 4,
+    title: 'Food Card',
+    img: 'https://react-coding-assignment.s3.ap-south-1.amazonaws.com/cards/black_card.png',
+    price: 21.00,
+    company: 'htc',
+    info: 'This card is used for spending on food merchants',
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Food Card",
+  img: "https://react-coding-assignment.s3.ap-south-1.amazonaws.com/cards/black_card.png",
+  price: 10.00,
+  company: "google",
+  info: 'This card is used for spending on food merchants',
+  inCart: false,
+  count: 0,
+  total: 0
+};
